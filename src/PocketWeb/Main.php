05fa87afcfa01f5c0ed4cd09d3f5d97b191baa3b
Michael Leahy <dutok@users.noreply.github.com>
2014-07-12 02:47:53 -0400

    Update Main.php
    This should work, but I haven't tested it.

--- src/PocketWeb/Main.php
+++ src/PocketWeb/Main.php
@@ -7,6 +7,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
+	
     private $WebsiteURL;
     
     public function onEnable() {
@@ -14,9 +15,19 @@ class Main extends PluginBase{
         $config = $this->getConfig();
         $this->WebsiteURL = $config->get("WebsiteURL");
         $this->getLogger()->info("PocketWeb has been enabled.");
-	}
+    }
 
     
-    public function onDisable(){
+    public function onDisable() {
         $this->getLogger()->info("PocketWeb has been disabled. Thanks for using PocketWeb");
     }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+	switch($command->getName()){
+            case "website":
+            	$sender->sendMessage("Our website URL is: ".$this->WebsiteURL);
+            	return true;
+            	break;
+	}
+    }
+}
