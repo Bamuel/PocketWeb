e6c6aad5f2867ef13108935e63ba5bdc27e0bcc1
MegaSamNinja <Samuel_ipad2@hotmail.com>
2014-07-12 12:52:07 +1000

    Update Main.php

--- src/PocketWeb/Main.php
+++ src/PocketWeb/Main.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace PocketWeb;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+    
+    public function onEnable(){
+        $this->getLogger()->info("Pocket Web has been enabled.");
+		    $this->perm = $this->getServer()->getPluginManager()->getPermission("website.command"); 
+    }
+    
+    public function onDisable(){
+        $this->getLogger()->info("PocketWeb has been disabled. Thanks for using PocketWeb");
+    }
