97df1b8c60b5276111912392f409cb0329fae54f
Samuel Levin <Samuel_ipad2@hotmail.com>
2014-07-12 13:14:35 +1000

    Updated the plugin (feeling so cool now)
    Shit im abit lost 
    
    work on it later

--- src/PocketWeb/Main.php
+++ src/PocketWeb/Main.php
@@ -7,11 +7,15 @@ use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
+    private $WebsiteURL;
     
-    public function onEnable(){
-        $this->getLogger()->info("Pocket Web has been enabled.");
-		    $this->perm = $this->getServer()->getPluginManager()->getPermission("website.command"); 
-    }
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $config = $this->getConfig();
+        $this->WebsiteURL = $config->get("WebsiteURL");
+        $this->getLogger()->info("PocketWeb has been enabled.");
+	}
+
     
     public function onDisable(){
         $this->getLogger()->info("PocketWeb has been disabled. Thanks for using PocketWeb");
